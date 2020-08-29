31bef685da7cae9534c2a4dfe96fd8d2e355605d
Happiness_Chung <hamdoong222@gmail.com>
2020-08-29 13:22:17 +0900

    initial commit

--- app/src/main/java/com/goldfish/dailyplanner/MainActivity.java
+++ app/src/main/java/com/goldfish/dailyplanner/MainActivity.java
@@ -11,4 +11,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //TODO : 이곳에서 작성한 정보를 저장
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 }
